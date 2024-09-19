14a46c94f3379be0f13eae67506e2c219aa6d2f2
crisdev <cris.cadosh@gmail.com>
2024-09-18 20:30:09 -0400

    Agregar archivos de proyecto.

---
+++ ListaSimpeEnlasada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSimpeEnlasada
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            SimpleLists list = new SimpleLists();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.ListarDatos();
+
+            list.Eliminar(2);
+            list.ListarDatos();
+            list.Eliminar(7);
+        }
+    }
+}
---
+++ ListaSimpeEnlasada/SimpleList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSimpeEnlasada
+{
+
+    public class Nodo
+    {
+        public int Datos;
+        public Nodo siguiente;
+
+        public Nodo(int dato)
+        {
+            this.Datos = dato;
+            this.siguiente = null; 
+        }
+    }
+
+
+    public class SimpleLists
+    {
+
+        private Nodo inicio;
+
+        public SimpleLists()
+        {
+            inicio = null;
+        }
+
+        public void Add (int data)
+        {
+            Nodo newNodo = new Nodo(data);
+
+            if(inicio == null)
+            {
+                inicio = newNodo;
+            }else
+            {
+                Nodo recorrido = inicio;
+                while(recorrido.siguiente != null)
+                {
+                    recorrido = recorrido.siguiente;
+                }
+
+                recorrido.siguiente = newNodo;
+            }
+        }
+
+        public void ListarDatos()
+        {
+
+            if(inicio == null)
+            {
+                Console.WriteLine("Lista vacia");
+                return;
+            }
+
+            Nodo recorrido = inicio;
+            Console.WriteLine("elementos de la listas");
+            while(recorrido != null)
+            {
+                Console.WriteLine(recorrido.Datos + "->");
+                recorrido = recorrido.siguiente;
+                
+            }
+            Console.WriteLine("vacio");
+
+        }
+
+        public void Eliminar(int data)
+        {
+            if (inicio == null)
+            {
+                Console.WriteLine("Lista vacia");
+                return;
+            }
+
+            if (inicio.Datos == data)
+            {
+                inicio = inicio.siguiente;
+                return;
+            }
+
+            Nodo recorrido = inicio;
+            Nodo nodoPrevio = null;
+
+            while (recorrido != null && recorrido.Datos != data)
+            {
+                nodoPrevio = recorrido;
+                recorrido = recorrido.siguiente;
+            }
+
+            if (recorrido == null)
+            {
+                Console.WriteLine("la info no esta en la lista");
+                return;
+            }
+
+            nodoPrevio.siguiente = recorrido.siguiente;
+        }
+
+    }
+}
